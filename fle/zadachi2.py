fef19651f8f8287bcb1467cba17027b0c161595d
rodarorada <ilusha.pristavusha@gmail.com>
2025-05-10 17:23:13 +0300

    1374:347829:892347

--- fle/zadachi2.py
+++ fle/zadachi2.py
@@ -1,10 +1,12 @@
-def dvenadcsati():
-    with open("test2.txt", "r") as file:
-            data=[int(x) for x in file.readlines()]
-            for i in data:
-                chet=[int(x) for x in data if x%2==0]
-                nechet = [int(x) for x in data if x % 2 == 1]
-                sp=[int(x) for x in str(i).split()]
-                print(chet, nechet)
-                # if sp.count(i)==1:
-dvenadcsati()
+# def dvenadcsati():
+#     with open("test2.txt", "r") as file:
+#             data=[int(x) for x in file.readlines()]
+#             for i in data:
+#                 chet=[int(x) for x in data if x%2==0]
+#                 nechet = [int(x) for x in data if x % 2 == 1]
+#                 sp=[int(x) for x in str(i).split()]
+#                 print(chet, nechet)
+#                 # if sp.count(i)==1:
+# dvenadcsati()
+
+print("bye world")
