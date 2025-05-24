b2b0e372af67717f81c6c1c4021ba16d8fdea5a4
rodarorada <ilusha.pristavusha@gmail.com>
2025-05-24 13:05:59 +0300

    dvadcat_odin():

---
+++ fle/zadachi3.py
@@ -0,0 +1,41 @@
+
+
+
+
+from itertools import *
+#
+# def shestnadchat():
+#     st="012345678"
+#     kol=0
+#     for i in product(st,repeat=12):
+#         if i[0]!="0":
+#             flag = True
+#             for j in range(len(i)-1):
+#                 if (i[j]+i[j+1]%2==0 and i[j]<i[j+1]) or (i[j]+i[j+1]%2==1 and i[j]>i[j+1]):
+#                     pass
+#                 else:
+#                     flag=False
+#             if flag==True:
+#                 kol=kol+1
+#     print(kol)
+# shestnadchat()
+
+# def vosemnadcat():
+#     st="0123456789abcde"
+#     bykvi="abcde"
+#     kol=0
+#     for i in product(st,repeat=8):
+#         if i.count("0")==2:
+#             kol_bykvi = 0
+#             for j in bykvi:
+#                 kol_bykvi=kol_bykvi+i.count(j)
+#             if kol_bykvi==2:
+#                 kol=kol+1
+#     print(kol)
+# vosemnadcat()
+
+def dvadcat_odin():
+    st="0123456"
+    kol=0
+    for i in product(st,repeat=5):
+        if int(i[0])%2==0:
